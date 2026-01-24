4bd5cdcfd258aa070cd810b0a18c0440b92f7199
lakshayvaishnav <thelakshayvaishnav@gmail.com>
2026-01-24 11:06:31 +0000

    added candles controller

---
+++ apps/api-service/src/schemas/candles.type.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const GetCandlesQuerySchema = z.object({
+  ts: z.string(),
+  startTime: z.coerce.number(),
+  endTime: z.coerce.number(),
+  asset: z.string(),
+});
