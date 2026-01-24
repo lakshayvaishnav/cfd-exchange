b6abf2b56491c2cecbaefed07244e818a00fd373
lakshayvaishnav <thelakshayvaishnav@gmail.com>
2026-01-24 06:01:04 +0000

    added auth routes

---
+++ apps/api-service/src/routes/auth.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { login, logout, register, me } from "../controllers/auth.controller";
+import { authenticate } from "../middlewares/authenticate";
+
+const router = Router();
+
+router.post("/login", login);
+router.post("/register", register);
+router.post("/logout", logout);
+router.get("/me", authenticate, me);
