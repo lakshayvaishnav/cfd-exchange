e5be0da5df7140f4ed34d5d7c1161fa38b2e82be
lakshayvaishnav <thelakshayvaishnav@gmail.com>
2026-01-24 10:38:50 +0000

    added balance routes

---
+++ apps/api-service/src/routes/balance.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+  depositBalance,
+  getBalance,
+  getBalanceByAsset,
+} from "../controllers/balance.controller";
+import { authenticate } from "../middlewares/authenticate";
+
+const router: Router = Router();
+router.use(authenticate);
+
+router.get("/", getBalance);
+router.get("/:symbol", getBalanceByAsset);
+router.post("/deposit", depositBalance);
+
+export default router;
