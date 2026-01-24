f43566cc86f1deebfd4107732d29b298ef426be5
lakshayvaishnav <thelakshayvaishnav@gmail.com>
2026-01-24 10:19:30 +0000

    fixed the prisma

---
+++ packages/database/index.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client.js";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    "DATABASE_URL is not defined. Please set it in your .env file.",
+  );
+}
+
+const connectionString = process.env.DATABASE_URL;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
