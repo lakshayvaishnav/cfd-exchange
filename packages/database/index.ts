b8f1d15a6dc978310f5db1df18b75adbf9e32344
lakshayvaishnav <thelakshayvaishnav@gmail.com>
2026-02-01 21:02:54 +0000

    added tanstack query

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -3,9 +3,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client.js";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL is not defined. Please set it in your .env file.",
-  );
+  throw new Error("DATABASE_URL is not defined. Please set it in your .env file.");
 }
 
 const connectionString = process.env.DATABASE_URL;
