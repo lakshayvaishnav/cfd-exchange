42630e3655d749d8112c2653fb1704a6d6bb394c
lakshayvaishnav <thelakshayvaishnav@gmail.com>
2026-02-02 08:54:43 +0000

    added docs, auth and header/hero section

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
-      {/* 
-      header section
-      hero section
-    */}
+      <Header />
+      <Hero />
     </div>
   );
 }
